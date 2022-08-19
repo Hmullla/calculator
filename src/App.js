959a9426763a2a225492f17f361cb26d79bb9f43
Hmullla <hannahmulla4@gmail.com>
2022-08-19 17:44:21 +0100

    finished calc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const digits = ['1','2','3','4','5','6','7','8','9','0'];
+  const operators = ['+','-','*','/'];
+
+  const [result, setResult] = useState("");
+
+  const handleClick = (button) => {
+    if(
+      operators.includes((button)) && result === '' ||
+      operators.includes((button)) && operators.includes(result.slice(-1))
+      )
+    {
+      return;
+    }
+    setResult(result + button);
+
+    if (!operators.includes((button))) {
+      setResult(eval(result + button).toString());
+    }
+  }
+
+  const calculate = () => {
+    setResult(eval(result).toString());
+  }
+
+  const deleteNums = () => {
+    if (result == ''){
+      return;
+    }
+
+    const button = result.slice(0, -1);
+
+    setResult((button));
+  }
+
+  const dig = digits.map(buttons => {
+    return <button name={digits} onClick={() => handleClick(buttons)}>{buttons}</button>;
+  });
+  const opp = operators.map(buttons => {
+    return <button name={operators} onClick={() => handleClick(buttons)}>{buttons}</button>;
+  });
+    return (
+      <div className='App'>
+        <div className='display'>
+          <p>{result || "0" }</p>
+        </div>
+        <div className='calc'>
+          {dig}
+          {opp}
+          <button onClick={deleteNums}>DEL</button>
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
+    );
+  }
+
 
 export default App;
